b21952259675962d40c5057d90927277e22c4688
therealtommy <sokolovai1887@ya.ru>
2025-11-05 21:44:11 +0700

    Add logic for scrolling questions

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -5,9 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,8 +25,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab4.ui.theme.Lab4Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GeoQuizApp(modifier = Modifier.padding(innerPadding))
+                    GeoQuizApp(innerPadding)
                 }
             }
         }
@@ -37,7 +53,7 @@ data class Question(
     val answer: Boolean
 )
 @Composable
-fun GeoQuizApp(modifier: Modifier) {
+fun GeoQuizApp(innerPadding: PaddingValues) {
     val questions = listOf(
         Question("Canberra is the capital of Australia.", true),
         Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
@@ -55,5 +71,66 @@ fun GeoQuizApp(modifier: Modifier) {
     val currentQuestion = questions[currentIndex]
     val isLastQuestion = currentIndex == questions.size - 1
 
+    Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+            Text(
+                text = currentQuestion.text,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(vertical = 32.dp),
+            )
 
+            if (!isAnswered) {
+                Button(
+                    onClick = {
+                        isAnswered = true
+                        if (currentQuestion.answer) score++
+                    },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text("True")
+                }
+
+                Button(
+                    onClick = {
+                        isAnswered = true
+                        if (!currentQuestion.answer) score++
+                    },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text("False")
+                }
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            if (isAnswered && !isLastQuestion) {
+                Button(
+                    onClick = {
+                        currentIndex++
+                        isAnswered = false
+                    }
+                ) {
+                    Text("Next Question")
+                }
+            }
+
+            if (isAnswered && isLastQuestion) {
+                Button(
+                    onClick = { showResult = true }
+                ) {
+                    Text("Show Results")
+                }
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab4Theme {
+        GeoQuizApp (innerPadding = PaddingValues())
+    }
 }
