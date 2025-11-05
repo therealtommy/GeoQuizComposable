228935ba49dec24db652b4134a569a78c5b20350
therealtommy <sokolovai1887@ya.ru>
2025-11-05 21:16:15 +0700

    Add statements for variables

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.lab4.ui.theme.Lab4Theme
 
 class MainActivity : ComponentActivity() {
@@ -39,4 +46,14 @@ fun GeoQuizApp(modifier: Modifier) {
         Question("The Amazon River is the longest river in the Americas.", true),
         Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
     )
+
+    var currentIndex by remember { mutableStateOf(0) }
+    var score by remember { mutableStateOf(0) }
+    var isAnswered by remember { mutableStateOf(false) }
+    var showResult by remember{ mutableStateOf(false) }
+
+    val currentQuestion = questions[currentIndex]
+    val isLastQuestion = currentIndex == questions.size - 1
+
+
 }
